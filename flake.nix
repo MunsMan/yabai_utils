74c763c245c8868ffa69d1477e3a1bf6c7b4df5f
MunsMan <munsman.github@gmail.com>
2024-04-17 18:27:29 +0200

    updaing nix flake to provide default package

--- flake.nix
+++ flake.nix
@@ -12,24 +12,21 @@
         pkgs = import nixpkgs { inherit system overlays; };
         rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
           toolchain.default.override { extensions = [ "rust-src" ]; });
-        darwinInputs = with pkgs;
-          lib.optionals stdenv.isDarwin
-            (with pkgs.darwin.apple_sdk.framework; [ ]);
       in
       with pkgs; {
         devShells.default = mkShell {
-          packages = [ rust rust-analyzer-unwrapped ] ++ darwinInputs;
+          packages = [ rust rust-analyzer-unwrapped ];
         };
-        packages.yabai-utils = rustPlatform.buildRustPackage {
-                pname = "yabai-utils";
-                version = "1.0.0";
-                src = ./.;
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "yabai-utils";
+          version = "1.0.0";
+          src = ./.;
 
-                cargoSha256 = "sha256-TPMXEJtrepZyA4efUQYa6i0SWglgSNEckOo34QOoOCU=";
+          cargoSha256 = "sha256-TPMXEJtrepZyA4efUQYa6i0SWglgSNEckOo34QOoOCU=";
 
-                buildInputs = [];
+          buildInputs = [ ];
 
-                };
+        };
       });
 }
 
